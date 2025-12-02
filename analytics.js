4b71f2fc5ed25f14e0f340f3fb638c2d17a5a674
pharmermichelle <michelle.waite@alumni.acphs.edu>
2025-12-02 13:56:32 -0500

    gtagging

---
+++ analytics.js
@@ -0,0 +1,8 @@
+// Google Analytics 4 global tag for ScreenITology
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+
+gtag("js", new Date());
+gtag("config", "G-3PJT7ZWXRR"); // <-- replace with your Measurement ID
